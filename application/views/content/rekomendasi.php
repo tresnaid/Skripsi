631b72e5c3f23264e759aa981319ee8dbb6b4b99
tresnaid <32062861+tresnaid@users.noreply.github.com>
2018-05-14 02:22:45 +0700

    css

--- application/views/content/rekomendasi.php
+++ application/views/content/rekomendasi.php
@@ -1,30 +1,27 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/roadmap/style.css">
 
  <section class="section-gray">
-	<div class="container clearfix">
+ 	<div class="container clearfix">
 		<div class="row services">
 		  	<div class="col-md-12">
 		    	<h2 class="heading">ROADMAP IMPLEMENTASI</h2>
+		    	<br><br><br><br>
 				<div class="container">
-					<div class="qa-message-list" id="wallmessages">
-						<?php foreach ($hasil as $row): ?>
-						<div class="message-item" id="m16">
-							<div class="message-inner">
-								<div class="message-head clearfix">
-									<div class="user-detail">
-										<h5 class="handle"><?php echo $row->isneed; ?></h5>
-										<div class="post-meta">
-											<div class="asker-meta">
-												<span class="qa-message-what"></span>
-												<span class="qa-message-when">
-													<span class="qa-message-when-data"><?php echo $row->Tipe ?></span>
-												</span>	
-											</div>
-										</div>
-									</div>
-								</div>
+				 	<div class="flex-parent">
+						<div class="input-flex-container">
+							<?php foreach ($hasil as $row): ?>
+							<div class="input">
+								<?php if ($row->Tipe == "Perbaharui"): ?>
+									<span data-info="<?php echo $row->detail ?> pada <?php echo $row->isneed ?>"></span>
+									<span data-year="tambah fitur <?php echo $row->detail ?>"></span>
+								<?php else: ?>
+									<span data-info="<?php echo $row->isneed ?>"></span>
+									<span data-year="<?php echo $row->Tipe ?>"></span>
+								<?php endif ?>
 							</div>
+								
+							<?php endforeach ?>
 						</div>
-						<?php endforeach ?>
 					</div>
 				</div>
 			</div>
@@ -36,6 +33,7 @@
   	<div class="container clearfix">
     	<div class="row services">
       		<div class="col-md-12">
+		    	<h2 class="heading">TABEL HASIL</h2>
         		<div class="row">
           			<div class="container" style="margin-top:50px">
               			<div class="panel-body" style="padding:0px">
@@ -43,11 +41,8 @@
                   				<thead>
 									<tr>
 										<th style="text-align:center;" width="20">NO</th>
-										<th>Alternatif</th>
+										<th>Kebutuhan Sistem Informasi</th>
 										<th>Tipe</th>
-										<!-- <?php foreach ($kriteria as $row): ?>
-											<th style="text-align:center;"><?php echo $row->nama_kriteria ?></th>
-										<?php endforeach ?> -->
 										<th style="text-align:center;">Nilai</th>
 									</tr>
 									</thead>
@@ -64,19 +59,6 @@
 											</tr>
 											<?php $counter++; ?>
 										<?php endforeach ?>
-											<!-- <?php foreach ($hasil as $key): ?>
-												<tr>
-													<td style="text-align:center;"><?php echo $counter ?></td>
-													<td><?php echo $key->isneed ?></td>
-													<td><?php echo $key->Tipe ?></td>
-													<td><?php echo $key->nilai_kriteria_1 ?></td>
-													<td><?php echo $key->nilai_kriteria_2 ?></td>
-													<td><?php echo $key->nilai_kriteria_3 ?></td>
-													<td><?php echo $key->nilai_kriteria_4 ?></td>
-													<td><?php echo $key->nilai ?></td>
-												</tr>
-											<?php $counter++; ?>
-										<?php endforeach ?> -->
 									</tbody>
 								</table>
 						</div>
